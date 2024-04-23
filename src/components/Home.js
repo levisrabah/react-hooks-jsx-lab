54ed749da61e3a1834729f868451d5fe72d0dbfe
LEVIS <levisrabah@gmail.com>
2024-04-23 22:39:32 +0300

    Completed lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,10 @@ import { name, city } from "../data/data.js";
 function Home() {
   return (
     <div id="home">
-      <h1 style={{color: "firebrick"}}>
-        {name} is a web Developer from {city}
-
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
       </h1>
-
     </div>
-
   );
 }
 
